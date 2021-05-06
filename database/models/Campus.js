29e0917fcb3d4dd98c9620de33421688707b44cb
Gabby <gabegabby99@gmail.com>
2021-05-06 17:17:39 -0400

    added campus and student model

--- database/models/Campus.js
+++ database/models/Campus.js
@@ -1,17 +1,33 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Campus = db.define("campus", {
+//Campus model -Gabby
+const campModel = {
+    name: "Hunter",
+    description: "description",
+    imageURL: null, //default photo here 
+    address: "address"
+}
 
+const Campus = db.define("campus", {
   name: {
     type: Sequelize.STRING,
     allowNull: false
   },
-
   description: {
     type: Sequelize.STRING,
-  }
+  },
+  /*
+  Idk, if I was doing this right.... 
 
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  address: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }*/
 });
 
 module.exports = Campus;
--- database/models/Student.js
+++ database/models/Student.js
@@ -1,6 +1,16 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
+//I added this model, not sure if i was supposed to add email, image, and gpa in const student
+const Students = {
+  student: {
+    firstName: "Gabby",
+    lastName: "Gonzalez",
+    email: "Gabriella.gonzalez90@myhunter.cuny.edu",
+    imageUrl: null,  //need default photo here
+    gpa: "4.0" 
+  }
+}
 const Student = db.define("student", {
 
   firstname: {
@@ -11,8 +21,23 @@ const Student = db.define("student", {
   lastname: {
     type: Sequelize.STRING,
     allowNull: false
-  }
+  }, 
+  /*
+  I added these but not sure if its right?? 
 
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  gpa: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+*/
 });
 
 module.exports = Student;
